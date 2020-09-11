80ec96733ff2725bf9a34c9b6e0402497cc186f2
Brenner Fagundes <brenner.araujo@grupolpj.com>
2020-09-11 12:09:38 -0300

    send xml

--- app/Http/Controllers/EnvioNotaServicoController.php
+++ app/Http/Controllers/EnvioNotaServicoController.php
@@ -187,9 +187,9 @@ class EnvioNotaServicoController extends AppBaseController
         'as_msg_nota'=>'teste',
         'ind_doc_eletronico' => 's',
         'ablb_xml'=>base64_encode(file_get_contents($file->getRealPath())) //new \CURLFILE('http://3.22.8.104:8082/storage/'.$destinationPath)
-    );
-        dd(json_encode($params));
-        dd(base64_encode(file_get_contents($file->getRealPath())));
+        );
+        $paramsJson = json_encode($params);
+
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
@@ -201,22 +201,8 @@ class EnvioNotaServicoController extends AppBaseController
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => array('as_dsc_extensao' => 'XML',
-        'as_doc_eletronico'=>'S',
-        'as_erro_nota'=>'N',
-        'as_msg_nota'=>'teste',
-        'ind_doc_eletronico' => 's',
-        'ablb_xml'=>base64_encode(file_get_contents($file->getRealPath())) //new \CURLFILE('http://3.22.8.104:8082/storage/'.$destinationPath)
-    ),
-
-
-        CURLOPT_HTTPHEADER => array(
-            "client_id: 7ef1d710-35c2-3aa1-82f8-6b82dc1b58d4",
-            "access_token:17895c05-6c3f-3d38-9065-bfa1b5294f1a",
-
-        ),
-       // CURLOPT_POSTFIELDS => array('dsc_extensao' => '.xml','con_arquivo_doc'=> new \CURLFILE('http://3.22.8.104:8082/storage/energisa%20teste/nota01.xlsx')),
-        ));
+        CURLOPT_POSTFIELDS => $paramsJson
+    ));
         //
 
         $response = curl_exec($curl);
